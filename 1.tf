eca691c74280b5a557e39c8b1c774ab955bcc82a
Iamneo <techadmin@iamneo.ai>
2023-09-22 08:30:26 +0000

    Backup Commit

--- 1.tf
+++ 1.tf
@@ -8,5 +8,10 @@ terraform{
 }
 provider "aws"{
     
-    region="ap-southeast-
+    region="ap-southeast-1"
+    access_key="AKIAQ6LJ7J6OUESCUTVH"
+    secret_key="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"
+}
+resource "aws_instance" "linux_server"{
+    ami=""
 }
